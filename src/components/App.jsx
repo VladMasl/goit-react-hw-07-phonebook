12ce75c6061a1a55e43495b5ddd78e51d8c72477
Vlad  Masliuk <102538039+VladMasl@users.noreply.github.com>
2022-08-16 14:23:41 +0300

    start HW

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
+import { useSelector } from 'react-redux';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import s from '../components/App.module.css';
+
+function App() {
+  const contacts = useSelector(state => state.contacts.item);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={s.decor}>
+      <h1>Phonebook📱</h1>
+      <ContactForm />
+      {contacts.length === 0 ? (
+        <h2>
+          You have no records☝
+          <br />
+          Enter the number and name☝
+        </h2>
+      ) : (
+        <>
+          <h2>Contacts</h2>
+          <Filter />
+          <ContactList />
+        </>
+      )}
     </div>
   );
-};
+}
+export default App;
